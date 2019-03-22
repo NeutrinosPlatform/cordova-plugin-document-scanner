92e65b50d4e9cfc91ffc1c58bc8289f1df254d42
ChrisTomAlxNeu <christy.alex@neutrinos.co>
2019-03-22 21:51:26 +0530

    breaking plugins one version at a time

---
+++ src/ios/Scan.swift
@@ -0,0 +1,75 @@
+import Foundation
+import WeScan
+
+var com = CDVInvokedUrlCommand()
+var uri = "";
+
+@objc(Scan) class Scan : CDVPlugin, ImageScannerControllerDelegate {
+    
+    
+    @objc(scanDoc:)
+    func scanDoc(_ command: CDVInvokedUrlCommand){
+        com = command
+
+        let scannerViewController = ImageScannerController()
+        scannerViewController.imageScannerDelegate = self
+        
+        // Get the top most view and start the camera
+        if var topController = UIApplication.shared.keyWindow?.rootViewController {
+            while let presentedViewController = topController.presentedViewController {
+                topController = presentedViewController
+            }
+            topController.present(scannerViewController, animated: true)
+        }
+    }
+    
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+        print(error)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: (error as? String));
+        commandDelegate.send(pluginResult, callbackId:com.callbackId);
+        scanner.dismiss(animated: true)
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+        if(saveImage(image: results.scannedImage)) {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: uri);
+            commandDelegate.send(pluginResult, callbackId:com.callbackId);
+        }
+        else {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "File creation failed!");
+            commandDelegate.send(pluginResult, callbackId:com.callbackId);
+        }
+        scanner.dismiss(animated: true)
+    }
+    
+    func saveImage(image: UIImage) -> Bool {
+        guard let data = UIImageJPEGRepresentation(image, 1) ?? UIImagePNGRepresentation(image) else {
+            return false
+        }
+        guard let directory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) as NSURL else {
+            return false
+        }
+        do {
+            try data.write(to: directory.appendingPathComponent("scanimg.png")!)
+            uri = directory.appendingPathComponent("scanimg.png")?.absoluteString ?? ""
+            if (uri == "") {
+                return false
+            }
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Cancelled");
+        commandDelegate.send(pluginResult, callbackId:com.callbackId);
+        scanner.dismiss(animated: true)
+    }
+    
+    override func pluginInitialize() {
+        super.pluginInitialize()
+    }
+}
--- www/scan.js
+++ www/scan.js
@@ -1,7 +1,17 @@
-/*global cordova, module*/
 
 module.exports = {
-    scanDoc: function (sourceType, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Scan", "scanDoc", [sourceType]);
+    scanDoc: function (successCallback, errorCallback, options) {
+        options = options || {};
+		options.sourceType = (options.sourceType == undefined) ? 1 : options.sourceType;
+    	if(options.sourceType === 1 || options.sourceType === 0)
+    	{
+			var sourceType = options.sourceType;	// 0 Gallery, 1 Camera
+			var args = [sourceType];
+        	cordova.exec(successCallback, errorCallback, "Scan", "scanDoc", args);
+    	}
+    	else
+    	{
+    		alert("sourceType value incorrect!");
+    	}
     }
 };
