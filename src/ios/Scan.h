96e8522e30a7e8f9d8e2f436ec2a23fe5fbd1088
Robert Schmidt <robert.schmidt2@kbs.de>
2021-03-05 10:16:01 +0100

    use custom WeScan library

--- src/ios/Scan.h
+++
@@ -1,8 +0,0 @@
-#import <Cordova/CDV.h>
-@import IRLDocumentScanner;
-
-@interface Scan : CDVPlugin <IRLScannerViewControllerDelegate>
-@property CDVInvokedUrlCommand* commandglo;
-- (void) scanDoc:(CDVInvokedUrlCommand*)command;
-
-@end
